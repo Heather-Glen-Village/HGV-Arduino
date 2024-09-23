fb11b4f359a36e931e3f78d69b02096cafbc1ca5
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-23 14:13:28 -0400

    made changes to Sendoary to see if that will work

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -1,3 +1,20 @@
 #define LED 2
 #define DI 0
 #define RO 1
+
+void setup(){
+    digitalWrite(LED, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    while(Serial.available())
+    {
+        char Message = Serial.read(); 
+        Serial.println(Message);
+        if(Message == 'A')
+        {
+            digitalWrite(LED, !digitalRead(LED));
+        }
+    }
+}
