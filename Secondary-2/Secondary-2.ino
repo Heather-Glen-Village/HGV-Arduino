1bbe51cd4f57aaf628a34f08a2787ccdbe28f70c
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-23 16:18:55 -0400

    Added SLAVE_EN

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -1,10 +1,13 @@
 #define LED 2
 #define DI 0
 #define RO 1
+#define SLAVE_EN 8
 
 void setup(){
     digitalWrite(LED, OUTPUT);
+    digitalWrite(SLAVE_EN, OUTPUT);
     Serial.begin(9600);
+    digitalWrite(SLAVE_EN, LOW);
 }
 
 void loop(){
