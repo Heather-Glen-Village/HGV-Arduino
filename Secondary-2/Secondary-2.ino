134944d6115035094d1ed324b2768f28e9731da6
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-23 11:21:36 -0400

    changing the board to upload it to

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -1,12 +1,15 @@
-const int led = 2;
-  
-void setup(){
-  pinMode(led, OUTPUT);
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(2, OUTPUT);
 }
-  
-void loop(){
-  digitalWrite(led, HIGH);
-  // delay(500);
-  // digitalWrite(led, LOW);
-  // delay(2500);
+
+void loop()
+{
+	digitalWrite(14, HIGH);
+    Serial.println("High");
+    delay(1000);
+    digitalWrite(2, LOW);
+    Serial.println("Low");
+    delay(1000);
 }
