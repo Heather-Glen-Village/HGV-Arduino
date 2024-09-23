8940d863495e0cfba5dd36ace8da838b33ddb9b0
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-23 14:08:33 -0400

    and this stuff

--- Secondary-2/build/sketch/Secondary-2.ino.cpp
+++ Secondary-2/build/sketch/Secondary-2.ino.cpp
@@ -1,28 +1,6 @@
 #include <Arduino.h>
 #line 1 "C:\\Users\\Zach_\\Documents\\Code\\HGV-Coop\\Rems006\\Secondary-2\\Secondary-2.ino"
 #define LED 2
-#define SLAVE_EN 1 // Change this to RS485 Pin
-  
-#line 4 "C:\\Users\\Zach_\\Documents\\Code\\HGV-Coop\\Rems006\\Secondary-2\\Secondary-2.ino"
-void setup();
-#line 9 "C:\\Users\\Zach_\\Documents\\Code\\HGV-Coop\\Rems006\\Secondary-2\\Secondary-2.ino"
-void loop();
-#line 4 "C:\\Users\\Zach_\\Documents\\Code\\HGV-Coop\\Rems006\\Secondary-2\\Secondary-2.ino"
-void setup(){
-  pinMode(LED, OUTPUT);
-  Serial.begin(9600);
-}
-  
-void loop(){
-    while(Serial.available()) // if there is something to read in the serial
-    {
-        digitalWrite(LED, LOW);
-        if(Serial.read() == 'A')
-        {
-            //digitalWrite(LED, !digitalRead(LED));
-            Serial.println("Message received");
+#define DI 0
+#define RO 1
 
-        }
-    }
-    digitalWrite(LED, HIGH);
-}
