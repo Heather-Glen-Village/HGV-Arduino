f9a1bfb99365eedf5de7b7458740c56fc24c385f
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-26 12:24:07 -0400

    added some backup folder of working code

---
+++ Basic-Receiver/Basic-Receiver.ino
@@ -0,0 +1,22 @@
+//#define DI 0
+//#define RO 1
+#define LED 2
+#define RsPower 8
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(RsPower, OUTPUT);
+    Serial.begin(9600);
+    digitalWrite(RsPower, LOW);
+}
+
+void loop(){
+    if(Serial.available() > 0){
+        String IncomingMessage = Serial.readString();
+        Serial.print("Received: ");
+        Serial.println(IncomingMessage);
+        if(IncomingMessage == "Test") {
+            digitalWrite(LED, !digitalRead(LED));
+        }
+    }
+}
---
+++ Basic-Sender/Basic-Sender.ino
@@ -0,0 +1,17 @@
+//#define DI 0
+//#define RO 1
+#define LED 2
+#define RsPower 8
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(RsPower, OUTPUT);
+    Serial.begin(9600);
+    digitalWrite(RsPower, HIGH);
+}
+
+void loop(){
+  Serial.print("Test"); // send a message 
+  Serial.flush(); // wait till Primary get the message
+  delay(5000);
+}
