edaee02bd96c5bbe1451c5067ff9a21a2f154081
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-27 11:53:13 -0400

    s2 sender s1 led

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -7,11 +7,8 @@ void setup() {
     pinMode(LED, OUTPUT);
     pinMode(RsPower, OUTPUT);
     Serial.begin(9600);
-    digitalWrite(RsPower, HIGH);
 }
 
 void loop(){
-  Serial.print("Test"); // send a message 
-  Serial.flush(); // wait till Primary get the message
-  delay(5000);
+    digitalWrite(LED, HIGH);
 }
