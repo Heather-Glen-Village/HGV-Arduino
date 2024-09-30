1595972805c7c0839c7053d2f879d4e67561823e
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-30 13:07:13 -0400

    changed ++ to +1

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -3,7 +3,7 @@
 #define LED 2
 #define DERE 8
 
-int DERE_POWER = 0; // Start Recieving
+int DERE_POWER = 0; // Start Sending
 int Count = 0;
 
 void setup() {
@@ -20,13 +20,14 @@ void loop(){
     digitalWrite(LED, DERE_POWER);
 
     if(Serial.available() > 0){ // Receiver Code
-        Count = Serial.parseInt() + 1;
+        Count = Serial.parseInt();
         DERE_POWER = 1;
         delay(1000);
     }
-    else if (DERE_POWER == 1){ // Sender Code
-        Serial.println(Count); // send a message 
+    else if (DERE_POWER == 1){  // Sender Code
+        Serial.println(Count+1); // send a message 
         Serial.flush(); // wait till other board get the message
         DERE_POWER = 0;
     }
+    
 }
--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -20,12 +20,12 @@ void loop(){
     digitalWrite(LED, DERE_POWER);
 
     if(Serial.available() > 0){ // Receiver Code
-        Count = Serial.parseInt() + 1;
+        Count = Serial.parseInt();
         DERE_POWER = 1;
         delay(1000);
     }
     else if (DERE_POWER == 1){  // Sender Code
-        Serial.println(Count); // send a message 
+        Serial.println(Count+1); // send a message 
         Serial.flush(); // wait till other board get the message
         DERE_POWER = 0;
     }
