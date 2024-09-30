e9e2ddcfd7d200d244402a3924d170f559a51ab4
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-09-30 11:55:46 -0400

    trying with s2

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -1,13 +1,14 @@
 //#define DI 0
 //#define RO 1
 #define LED 2
-#define RsPower 8
+#define DERE 8
 
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(DERE, OUTPUT);
     Serial.begin(9600);
     digitalWrite(DERE, LOW);
+    digitalWrite(LED, 1);
 }
 
 void loop(){
