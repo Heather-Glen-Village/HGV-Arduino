6bf26683add38b71aeb51c63fff27f9dec915418
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-01 11:52:04 -0400

    testing if match changes work

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -2,22 +2,34 @@
 //#define RO 1
 #define LED 2
 #define DERE 8
-#define DERE_POWER LOW
+
+int DERE_POWER = 1; // Start Receiving
+int Count = 0;
 
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(DERE, OUTPUT);
     Serial.begin(9600);
+
     digitalWrite(DERE, DERE_POWER);
+    digitalWrite(LED, LOW);
 }
 
 void loop(){
-    if(Serial.available() > 0){
-        String IncomingMessage = Serial.readString();
-        Serial.print("Received: ");
-        Serial.println(IncomingMessage);
-        if(IncomingMessage == "Test") {
-            digitalWrite(LED, !digitalRead(LED));
-        }
+    digitalWrite(DERE, DERE_POWER);
+    //digitalWrite(LED, DERE_POWER);
+    delay(1000);
+    if (DERE_POWER == 1){
+        Serial.println(Count);
+        Serial.flush();
+        DERE_POWER = 0;
+    }
+    else if(Serial.available() != 0){
+        Count = Serial.parseInt();
+        Serial.print("Got: ");
+        Serial.println(Count);
+        Serial.print("Sending: ");
+        Count++;
+        DERE_POWER = 1;
     }
 }
--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -3,33 +3,12 @@
 #define LED 2
 #define DERE 8
 
-int DERE_POWER = 1; // Start Receiving
-int Count = 0;
-
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(DERE, OUTPUT);
     Serial.begin(9600);
-
-    digitalWrite(DERE, DERE_POWER);
-    digitalWrite(LED, LOW);
 }
 
 void loop(){
-    digitalWrite(DERE, DERE_POWER);
-    //digitalWrite(LED, DERE_POWER);
-    delay(1000);
-    if (DERE_POWER == 1){
-        Serial.println(Count);
-        Serial.flush();
-        DERE_POWER = 0;
-    }
-    else if(Serial.available() != 0){
-        Count = Serial.parseInt();
-        Serial.print("Got: ");
-        Serial.println(Count);
-        Serial.print("Sending: ");
-        Count++;
-        DERE_POWER = 1;
-    }
+    digitalWrite(LED, HIGH);
 }
