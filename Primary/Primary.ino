e8bc5a4a3079c91b74b4389ae8fc5b25deed7125
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-02 13:17:55 -0400

    New mac and Ip

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -2,10 +2,10 @@
 #include <Ethernet.h>
 
 // Use a random, unique MAC address
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+byte mac[] = { 0xBE, 0x3D, 0x8E, 0xEF, 0xFE, 0xED };
 
 // Set a static IP address (ensure it's within your network's range)
-IPAddress ip(192, 168, 3, 177); // Adjust to match your network
+IPAddress ip(192, 168, 3, 87); // Adjust to match your network
 
 EthernetClient client;
 
