c4277df3864c5907f885b7672a81d2deb4b0d325
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 12:03:38 -0400

    Creating an Board Finder to find boards

---
+++ Find-Board/Primary/Primary.ino
@@ -0,0 +1,14 @@
+//#define DI 0
+//#define RO 1
+#define LED 2
+#define DERE 9
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(DERE, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    digitalWrite(LED, LOW);
+}
---
+++ Find-Board/Secondary-2/Secondary-2.ino
@@ -0,0 +1,17 @@
+//#define DI 0
+//#define RO 1
+#define LED 2
+#define DERE 9
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(DERE, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop(){
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
+}
