32f063074a16d0757bddcb245c036e1315a77389
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 12:10:10 -0400

    P Sender  both S are Reciver

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -2,13 +2,22 @@
 //#define RO 1
 #define LED 2
 #define DERE 9
+#define DERE_POWER LOW
 
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(DERE, OUTPUT);
     Serial.begin(9600);
+    digitalWrite(DERE, DERE_POWER);
 }
 
 void loop(){
-    digitalWrite(LED, HIGH);
+    if(Serial.available() > 0){
+        String IncomingMessage = Serial.readString();
+        Serial.print("Received: ");
+        Serial.println(IncomingMessage);
+        if(IncomingMessage == "Test") {
+            digitalWrite(LED, !digitalRead(LED));
+        }
+    }
 }
