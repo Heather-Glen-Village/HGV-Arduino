505b212f67266ba0c27e0658143ff1ddd764f894
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 11:26:46 -0400

    trying with static ip

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -1,11 +1,11 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-// Use a random, unique MAC address
-byte mac[] = { 0xBE, 0x3D, 0x8E, 0xEF, 0xFE, 0xED };
+// Use a random, unique MAC address C2:A2:DF:70:D8:44
+byte mac[] = { 0xC2, 0xA2, 0xDF, 0x70, 0xD8, 0x44 };
 
 // Set a static IP address (ensure it's within your network's range)
-//IPAddress ip(192, 168, 3, 87); // Adjust to match your network
+IPAddress ip(192, 168, 3, 87); // Adjust to match your network
 
 EthernetClient client;
 
@@ -14,7 +14,7 @@ void setup() {
   Serial.begin(9600);
 
   // start the Ethernet connection with a static IP:
-  Ethernet.begin(mac);
+  Ethernet.begin(mac, ip);
 
   // print your local IP address:
   Serial.print("Static IP Address: ");
