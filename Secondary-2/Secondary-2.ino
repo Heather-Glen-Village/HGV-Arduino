14744d061a808e5cf56bcc3d5b9363457028e406
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-03 11:55:06 -0400

    Testing Which Board goes where

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -3,18 +3,15 @@
 #define LED 2
 #define DERE 9
 
-#define DERE_POWER HIGH
 void setup() {
     pinMode(LED, OUTPUT);
     pinMode(DERE, OUTPUT);
     Serial.begin(9600);
-    
-    digitalWrite(DERE, DERE_POWER);
-    digitalWrite(LED, HIGH);
 }
 
 void loop(){
-  Serial.print("Test"); // send a message 
-  Serial.flush(); // wait till Primary get the message
-  delay(5000);
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
 }
