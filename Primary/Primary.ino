53ee386780570cd646e8aff3b48212e27680dee3
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-04 13:01:52 -0400

    plus this

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -1,5 +1,5 @@
-#include <NeoSWSerial.h>
-
+#include <SoftwareSerial.h>
+//#include <NeoSWSerial.h>
 #include <ModbusRTUMaster.h>
 
 // Pins List
@@ -8,8 +8,9 @@
 #define RX    15 //Phyical RX 1
 #define DERE  9
 #define LED   2
+SoftwareSerial modbusSerial(RX, TX);
+ModbusRTUMaster modbus(modbusSerial, DERE); // Create Modbus Object with port for RS485
 
-ModbusRTUMaster modbus(Serial, DERE); // Create Modbus Object with port for RS485
 
 bool SlaveLED = 1; // Enable
 
@@ -18,11 +19,15 @@ void setup() {
   digitalWrite(LED, SlaveLED);
   //modbus.setTimeout(500);
   modbus.begin(9600); // Baud Rate  | Config?
+  Serial.begin(9600);
 }
 
 void loop() {
   if (modbus.writeSingleCoil(1, 0, SlaveLED) == 0) {
     SlaveLED = !SlaveLED;
+  }
+  else{
+    Serial.println(modbus.getExceptionResponse());
   }
   digitalWrite(LED, SlaveLED);
   delay(2000);
