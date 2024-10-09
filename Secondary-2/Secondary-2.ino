2b71b8f5777799431f3c36738b013f036f1d34a4
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-09 11:59:14 -0400

    Modbus working?

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -1,47 +1,37 @@
-#define buzzerPin 2
+#include <ModbusRTUSlave.h>
 
-void setup() {
-  pinMode(buzzerPin, OUTPUT);  // Set buzzer pin as output
+// Pins List
+// #define SoftTX 14 // Phyical TX 0
+// #define SoftRX 15 // Phyical RX 1
+#define DERE 9
+#define LED 2
 
-  // Quater Note
-  digitalWrite(buzzerPin, HIGH); 
-  delay(272);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(272);
-  // 2 Eighth Notes
-  digitalWrite(buzzerPin, HIGH); 
-  delay(136);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(136);
+// Defines the ID for the Secondary Board from 1-246
+#define ID 2
 
-  digitalWrite(buzzerPin, HIGH); 
-  delay(136);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(136);
-  // Quater Note
-  digitalWrite(buzzerPin, HIGH); 
-  delay(272);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(272);
-  // Half Note
-  digitalWrite(buzzerPin, HIGH); 
-  delay(545);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(272);
-  // 1 Quater Rest
-delay(272);
-  // Quater Note
-  digitalWrite(buzzerPin, HIGH); 
-  delay(272);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(272);
-// Quater Note
-  digitalWrite(buzzerPin, HIGH); 
-  delay(272);                    
-  digitalWrite(buzzerPin, LOW);  
-  delay(272);
+// Initialize Library
+ModbusRTUSlave modbus(Serial, DERE); // Create Modbus Object
+
+bool coils[1]; // Creating an array where the Coils can go | Read & Write Only Bools
+
+void setup()
+{
+    pinMode(LED, OUTPUT);
+
+    modbus.configureCoils(coils, 1); // Says where The Coils can go and how many Value is allowed
+    modbus.begin(ID, 9600);          // ID | Baud Rate
+    Serial.begin(9600);              // For Debuging
 }
 
-void loop() {
-  // Empty loop, logic is in setup()
+void loop()
+{
+    if (Serial.available() != 0) // Check if There been any Request
+    {
+        Serial.println("LED Change"); // Debugging Line
+        modbus.poll();                // Check and act on the request from the Master
+
+        digitalWrite(LED, coils[0]); // Changes LED to Match with new Message
+    }
+
+    delay(500);
 }
