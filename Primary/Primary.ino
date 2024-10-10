d470e2c9c91b5d98e487cc3367292833d613d68b
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-10 14:17:25 -0400

    lower for loop

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -32,7 +32,7 @@ void loop()
   uint16_t returncode = modbus.readHoldingRegisters(1, 0, randomfloat_UINT16, 1);
   Serial.println(returncode);
   if(returncode == 0) {
-    for (int i = 0; i <= 200; i++) {
+    for (int i = 0; i <= 20; i++) {
       Serial.println(randomfloat[i]);
     }
     delay(3000);
