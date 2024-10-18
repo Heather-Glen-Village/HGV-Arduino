a50ceff05aa9a1c1f790511a56c94061b24821be
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-18 14:14:02 -0400

    Reapply "the starting of the temp storage system"
    This reverts commit 5dd8d9f922d4fd3216a22fd749db625cff02562d.

---
+++ Database/ModbusTCP.py
@@ -0,0 +1,20 @@
+from pyModbusTCP.client import ModbusClient
+from time import sleep
+
+Gateway_IP = "192.168.1.254"
+Board_ID = 0
+
+try: 
+    clinet = ModbusClient(host="192.168.1.254", unit_id=Board_ID, auto_open=True, auto_close=False) 
+except ValueError:
+    print(f"Could not connect to Modbus server: {ValueError}")
+    
+    
+while True:
+    Input_Registers = clinet.read_input_registers(0,2)
+    
+    if Input_Registers:
+        print(Input_Registers[0])
+        print(Input_Registers[1])
+    else:
+        print("Error reading input registers")
