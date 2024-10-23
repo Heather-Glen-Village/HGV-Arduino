b2c65dd2ccea384cb22eebbd28cf4af3be6c9f4a
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-10-23 12:46:16 -0400

    changed var name?

--- Primary/Primary.ino
+++ Primary/Primary.ino
@@ -4,26 +4,26 @@
 // MISO 12
 // SCK 13
 
-int SS = 10;
+int SlaveSelect = 10;
 
 #include <SPI.h>
 
 void setup() {
   Serial.begin(9600);
   SPI.begin();  // Initialize SPI
-  pinMode(SS, OUTPUT);  // Ensure SS is set as output
-  digitalWrite(SS, HIGH);  // Deselect the slave initially
+  pinMode(SlaveSelect, OUTPUT);  // Ensure SS is set as output
+  digitalWrite(SlaveSelect, HIGH);  // Deselect the slave initially
 
   Serial.println("SPI test started");
 }
 
 void loop() {
-  digitalWrite(SS, LOW);  // Select the slave
+  digitalWrite(SlaveSelect, LOW);  // Select the slave
 
   // Send a byte (0xAA) and receive a byte
   byte receivedData = SPI.transfer(0xAA);
 
-  digitalWrite(SS, HIGH);  // Deselect the slave
+  digitalWrite(SlaveSelect, HIGH);  // Deselect the slave
 
   // Print the received data
   Serial.print("Received: 0x");
