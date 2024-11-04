f0c6765f03d6c56669e667463eb2fd3bb2a64fc4
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-04 12:07:25 -0500

    S1 now Sending

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -1,45 +1,30 @@
 #include <SoftwareSerial.h>
 
-#define SoftDI 16 // A2
-#define SoftRO 17 // A3
+#define SoftDI 14 // A0
+#define SoftRO 15 // A1
 #define LED 2
 #define DERE 9
-#define DERE_POWER LOW
+#define DERE_POWER HIGH
 
 SoftwareSerial RS485Serial(SoftRO, SoftDI); // RX TX
 
 void setup()
 {
-  pinMode(LED, OUTPUT);
-  pinMode(DERE, OUTPUT);
-  Serial.begin(9600);
-  RS485Serial.begin(9600);
-  digitalWrite(DERE, DERE_POWER);
+    pinMode(LED, OUTPUT);
+    pinMode(DERE, OUTPUT);
+    Serial.begin(9600);
+    RS485Serial.begin(9600);
+    digitalWrite(DERE, DERE_POWER);
+    digitalWrite(LED, HIGH);
+    delay(5000);
 }
 
 void loop()
 {
-  Serial.print(Serial.available());
-  Serial.print(RS485Serial.available());
-  if (Serial.available() > 0)
-  {
-    String IncomingMessage = Serial.readString();
-    Serial.print("Received: ");
-    Serial.println(IncomingMessage);
-    if (IncomingMessage == "Test")
-    {
-      digitalWrite(LED, !digitalRead(LED));
-    }
-  }
-  else if (RS485Serial.available() > 0)
-  {
-    String IncomingMessage = RS485Serial.readString();
-    Serial.print("Received: ");
-    Serial.println(IncomingMessage);
-    if (IncomingMessage == "Test")
-    {
-      digitalWrite(LED, !digitalRead(LED));
-    }
-  }
-  delay(500);
+    Serial.print("Test"); // send a message
+    Serial.flush();       // wait till Primary get the message
+    delay(1000);
+    RS485Serial.print("Test"); // send a message
+    RS485Serial.flush();       // wait till Primary get the message
+    delay(5000);
 }
