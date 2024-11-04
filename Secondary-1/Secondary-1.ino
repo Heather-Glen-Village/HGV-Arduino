119e02f4474e34a51c0cde112ef8177692402fe3
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-04 12:29:16 -0500

    was the power set off?

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -1,7 +1,7 @@
 #include <SoftwareSerial.h>
 
-#define SoftDI 14 // A0
-#define SoftRO 15 // A1
+#define SoftDI 16 // A2
+#define SoftRO 17 // A3
 #define LED 2
 #define DERE 9
 #define DERE_POWER HIGH
@@ -10,21 +10,36 @@ SoftwareSerial RS485Serial(SoftRO, SoftDI); // RX TX
 
 void setup()
 {
-    pinMode(LED, OUTPUT);
-    pinMode(DERE, OUTPUT);
-    Serial.begin(9600);
-    RS485Serial.begin(9600);
-    digitalWrite(DERE, DERE_POWER);
-    digitalWrite(LED, HIGH);
-    delay(5000);
+  pinMode(LED, OUTPUT);
+  pinMode(DERE, OUTPUT);
+  Serial.begin(9600);
+  RS485Serial.begin(9600);
+  digitalWrite(DERE, DERE_POWER);
 }
 
 void loop()
 {
-    Serial.print("Test"); // send a message
-    Serial.flush();       // wait till Primary get the message
-    delay(1000);
-    RS485Serial.print("Test"); // send a message
-    RS485Serial.flush();       // wait till Primary get the message
-    delay(5000);
+  Serial.print(Serial.available());
+  Serial.print(RS485Serial.available());
+  if (Serial.available() > 0)
+  {
+    String IncomingMessage = Serial.readString();
+    Serial.print("Received: ");
+    Serial.println(IncomingMessage);
+    if (IncomingMessage == "Test")
+    {
+      digitalWrite(LED, !digitalRead(LED));
+    }
+  }
+  else if (RS485Serial.available() > 0)
+  {
+    String IncomingMessage = RS485Serial.readString();
+    Serial.print("Received: ");
+    Serial.println(IncomingMessage);
+    if (IncomingMessage == "Test")
+    {
+      digitalWrite(LED, !digitalRead(LED));
+    }
+  }
+  delay(500);
 }
