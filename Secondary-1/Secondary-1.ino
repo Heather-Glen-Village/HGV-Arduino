13a2b2cf36128ec9c29eceea5ae17b3dd71e64cd
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-05 11:47:56 -0500

    wait maybe

--- Secondary-1/Secondary-1.ino
+++ Secondary-1/Secondary-1.ino
@@ -22,26 +22,26 @@ void loop() {
   int Soft_A = RS485Serial.available();
   Serial.print(Serial_A);
   Serial.print(Soft_A);
-  if (Serial_A > 0)
-  {
-    String IncomingMessage = Serial.readString();
-    Serial.print("Received: ");
-    Serial.println(IncomingMessage);
-    
-    bool led_power = !digitalRead(LED);
-    Serial.println(led_power);
-    digitalWrite(LED, led_power);
-
-  }
-  // else if (Soft_A > 0)
+  // if (Serial_A > 0)
   // {
-  //   String IncomingMessage = RS485Serial.readString();
+  //   String IncomingMessage = Serial.readString();
   //   Serial.print("Received: ");
   //   Serial.println(IncomingMessage);
     
   //   bool led_power = !digitalRead(LED);
   //   Serial.println(led_power);
   //   digitalWrite(LED, led_power);
+
   // }
+  if (Soft_A > 0)
+  {
+    String IncomingMessage = RS485Serial.readString();
+    Serial.print("Received: ");
+    Serial.println(IncomingMessage);
+    
+    bool led_power = !digitalRead(LED);
+    Serial.println(led_power);
+    digitalWrite(LED, led_power);
+  }
   delay(500);
 }
