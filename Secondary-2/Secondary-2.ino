93e8f2e2beaa51487d03f2208096366dcecac18b
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-05 11:58:25 -0500

    S1 <- S2 somehow

--- Secondary-2/Secondary-2.ino
+++ Secondary-2/Secondary-2.ino
@@ -4,46 +4,25 @@
 #define SoftRO 17 // A3
 #define LED 2
 #define DERE 9
-#define DERE_POWER LOW
+#define DERE_POWER HIGH
 
 SoftwareSerial RS485Serial(SoftRO, SoftDI); // RX TX
 
-void setup()
-{
-  pinMode(LED, OUTPUT);
-  pinMode(DERE, OUTPUT);
-  Serial.begin(9600);
-  RS485Serial.begin(9600);
-  digitalWrite(DERE, DERE_POWER);
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    pinMode(DERE, OUTPUT);
+    Serial.begin(9600);
+    RS485Serial.begin(9600);
+    
+    digitalWrite(DERE, DERE_POWER);
+    digitalWrite(LED, HIGH);
 }
 
-void loop() {
-  int Serial_A = Serial.available();
-  int Soft_A = RS485Serial.available();
-  Serial.print(Serial_A);
-  Serial.print(Soft_A);
-  if (Serial_A > 0)
-  {
-    String IncomingMessage = Serial.readString();
-    Serial.print("Received: ");
-    Serial.println(IncomingMessage);
-    
-    bool led_power = !digitalRead(LED);
-    Serial.println(led_power);
-    digitalWrite(LED, led_power);
+void loop(){
+  Serial.print("Test"); // send a message 
+  delay(1000);
+  RS485Serial.print("Test"); // send a message
+  delay(5000);
 
-  }
-  if (Soft_A > 0)
-  {
-    Serial.println("We Got Something on Soft Serial");
-    Serial.println(RS485Serial.readString());
-    String IncomingMessage = RS485Serial.readString();
-    Serial.print("Received: ");
-    Serial.println(IncomingMessage);
-    
-    bool led_power = !digitalRead(LED);
-    Serial.println(led_power);
-    digitalWrite(LED, led_power);
-  }
-  delay(500);
 }
