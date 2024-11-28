23dc6b01553db3a6e2cc3331153d56113e9c4958
Zach Schultz <Zach.Schultz.2.0.1@gmail.com>
2024-11-28 13:49:43 -0500

    New Board Finder Code

--- Code-Collection/Board-Finder/Board-Finder.ino
+++ Code-Collection/Board-Finder/Board-Finder.ino
@@ -1,5 +1,11 @@
+#include <Ethernet.h>
+
 #define LED 2
 
+
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
 void setup()
 {
   pinMode(LED, OUTPUT);
@@ -8,8 +14,24 @@ void setup()
   digitalWrite(LED, HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
   Serial.println("Board Finder Started!");
+
+  Serial.println("Testing for Ethernet...");
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    } else if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+    // no point in carrying on, so do nothing forevermore:
+      Serial.println("Board is most likely not the Priamry One");
+  } else {
+    Serial.println(Ethernet.localIP());
+    Serial.println("This is a Priamry Board");
+  }
   Serial.println("Press Enter into the Terminal to Switch LED");
   Serial.println("Enter Character To cause the LED to Flash");
+
 }
 
 void loop() {
