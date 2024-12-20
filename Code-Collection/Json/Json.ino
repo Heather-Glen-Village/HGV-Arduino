12778d0d1b5d3e1108274e933664f9613ac50fca
Zach Schultz <Zach.Schultz.2.0.1@gmai.com>
2024-12-20 15:46:19 -0500

    ok from Scratch Json

--- Code-Collection/Json/Json.ino
+++ Code-Collection/Json/Json.ino
@@ -9,6 +9,26 @@ float FloatRegisters[2][3] = {
     {13.5, 23.5, 12}};
 
 bool smoke = 1;
+
+void sendData(bool discreteInputs[2][3], float FloatRegisters[2][6 / 2], bool Smoke)
+{
+    JsonDocument doc;
+    char SerialJson;
+
+    doc["Primary"] = 1;
+    doc["Secondary"] = 2;
+
+    doc["IR"][0] = 12.53;
+    doc["IR"][1] = 13.53;
+    doc["IR"][2] = 16.53;
+
+    doc["DI"][0] = 0;
+    doc["DI"][1] = 1;
+    doc["DI"][2] = 0;
+
+    serializeJson(doc, SerialJson);
+    Serial.println(SerialJson);
+}
 void setup()
 {
     // put your setup code here, to run once:
@@ -20,50 +40,4 @@ void loop()
     // put your main code here, to run repeatedly:
     sendData(discreteInputs, FloatRegisters, smoke);
     delay(5000);
-}
-
-void sendData(bool discreteInputs[2][3], float FloatRegisters[2][6 / 2], bool Smoke)
-{
-    // turn data into a Json and Send it to NodeRed
-
-    StaticJsonDocument<512> doc; // We going to need a bigger Json
-    char SensorJson[512];        // should be like 10%  bigger then the JSON I Think
-
-    Serial.println("Sending Data to NodeRed");
-    // Create Json for Every Secondary
-    for (int s = 0; s < 2; s++)
-    {
-        doc["Primary"] = 1;
-        doc["Secondary"] = s + 1;
-
-        // Float Register
-        JsonArray FR = doc.createNestedArray("FR");
-        for (int a = 0; a < 6 / 2; a++)
-        {
-            FR.add(FloatRegisters[s][a]);
-        }
-
-        // Discrete Inputs
-        JsonArray DI = doc.createNestedArray("DI");
-        for (int a = 0; a < 3; a++)
-        {
-            DI.add(discreteInputs[s][a]);
-        }
-
-        // Serialize JSON to a string and print it
-        Serial.println();
-        serializeJson(doc, SensorJson);
-        Serial.println(SensorJson);
-        // client.publish(SensorTopic, SensorJson);
-        doc.clear();
-    }
-    // Json for the Primary
-    doc["Primary"] = 1;
-    doc["Smoke"] = Smoke;
-
-    Serial.println();
-    serializeJson(doc, SensorJson);
-    Serial.println(SensorJson);
-    // client.publish(SensorTopic, SensorJson);
-    doc.clear();
 }
